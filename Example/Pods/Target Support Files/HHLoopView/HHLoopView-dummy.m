bfc276caeae6464615af44bf144aa6d88d176bb6
hehai <hehai682@126.com>
2018-08-01 11:37:36 +0800

    update .gitignore

--- Example/Pods/Target Support Files/HHLoopView/HHLoopView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HHLoopView : NSObject
-@end
-@implementation PodsDummy_HHLoopView
-@end
--- Example/Pods/Target Support Files/Pods-HHLoopView_Example/Pods-HHLoopView_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HHLoopView_Example : NSObject
-@end
-@implementation PodsDummy_Pods_HHLoopView_Example
-@end
